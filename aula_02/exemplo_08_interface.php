0ffd8383fcdbae699bb603b3b2b5df13d18d66fd
Gill Velleda Gonzales <gillvelleda@gmail.com>
2023-08-11 11:24:27 -0300

    feat: exemplo autoload separado

--- aula_02/exemplo_08_interface.php
+++ aula_02/exemplo_08_interface.php
@@ -1,8 +1,5 @@
 <?php 
-spl_autoload_register(function ($class_name){
-	$path = implode("/",explode("\\",$class_name)); //Unix
-	require_once "$path.php";
-});
+include 'autoload.php';
 
 use classes\Atleta;
 use classes\logs\Relatorio;
