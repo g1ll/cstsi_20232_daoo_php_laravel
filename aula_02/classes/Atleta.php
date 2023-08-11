bb6789123538e62e3e207927f88e1e3262e265ea
Gill Velleda Gonzales <gillvelleda@gmail.com>
2023-08-11 08:52:03 -0300

    feat: exemplos herança em php

---
+++ aula_02/classes/Atleta.php
@@ -0,0 +1,11 @@
+<?php
+require_once "classes/Pessoa.php";
+
+class Atleta extends Pessoa{
+
+	function __construct($nome, $altura, $peso)
+	{
+		parent::__construct($nome, null, $peso, $altura);
+		$this->calcIMC();
+	}
+}
