0a8ff77fe589fd38aaf8dc4f9a7d89dc5e7f8eeb
Gill Velleda Gonzales <gillvelleda@gmail.com>
2023-08-24 13:19:17 -0300

    refactor: remocao de espacos

--- aula_03/src/controller/App.php
+++ aula_03/src/controller/App.php
@@ -5,14 +5,11 @@ use Dotenv\Dotenv;
 
 class App{
 	public static function init() :void {
-
 		$path=__DIR__."/../../";
 		$dotenv = Dotenv::createImmutable($path);
 		$dotenv->load();
 		error_log("DIR:\n".__DIR__."\n");
 		include_once(__DIR__."/../config/routes.php");
-
-		// include_once "config/routes.php";
 		Route::routes($routes);
 ;	}
 }
