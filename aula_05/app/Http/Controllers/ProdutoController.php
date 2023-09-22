1f06e34bc310285d9ad5f19704686625b98d4877
Gill Velleda Gonzales <gillvelleda@gmail.com>
2023-09-22 09:41:49 -0300

    feat: metodos de criacao de novos produtos. create -  view com formulario. store - receber e cadastrar dados Aula 06 - Create (Crud)

--- aula_05/app/Http/Controllers/ProdutoController.php
+++ aula_05/app/Http/Controllers/ProdutoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Produto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
 class ProdutoController extends Controller
@@ -25,5 +26,19 @@ class ProdutoController extends Controller
         return view('produto-show',[
             'produto'=>Produto::find($id)
         ]);
+    }
+
+    public function create(): View{
+        return view('produto-create');
+    }
+
+    public function store(Request $request) {
+        $newProduto = $request->all();//array associativo
+        $newProduto['importado'] = $request->has('importado');
+        // dd($newProduto);
+        if(!Produto::create($newProduto))
+            dd("Erro ao inserir o novo produto!!!");
+
+        return redirect('/produtos');
     }
 }
