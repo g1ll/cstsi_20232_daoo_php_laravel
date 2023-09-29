74d00630f2901b0d337cd76a98c7b2a6f81464a2
Gill Velleda Gonzales <gillvelleda@gmail.com>
2023-09-29 09:17:58 -0300

    feat: create model regiao Criacao da model, migration e seeder de Regiao. php artisan make:model Regiao -ms

---
+++ aula_05/app/Models/Regiao.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Regiao extends Model
+{
+    protected $table='regioes';
+    protected $fillable = ['nome'];
+
+    // public function estados(){
+    //     return $this->hasMany(Estado::class);
+    // }
+}
